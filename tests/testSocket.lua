57994621fb44b14240fe13da968c6a71e12c1a49
Max Chase <max.chase@gmail.com>
2017-04-14 18:25:53 -0400

    Try changing hostname to IP? I'm not sure what Travis's deal is.

--- tests/testSocket.lua
+++ tests/testSocket.lua
@@ -1,6 +1,6 @@
 local log_sock = require"logging.socket"
 
-local logger = log_sock("localhost", 5000)
+local logger = log_sock("127.0.0.1", 5000)
 
 assert(logger:info("logging.socket test"))
 assert(logger:debug("debugging..."))
