51f0235e89ab884ac57fc804b50460f6f522b892
Max Chase <max.chase@gmail.com>
2017-04-14 18:23:52 -0400

    Add asserts to socket logging test.

--- tests/testSocket.lua
+++ tests/testSocket.lua
@@ -2,9 +2,9 @@ local log_sock = require"logging.socket"
 
 local logger = log_sock("localhost", 5000)
 
-logger:info("logging.socket test")
-logger:debug("debugging...")
-logger:error("error!")
+assert(logger:info("logging.socket test"))
+assert(logger:debug("debugging..."))
+assert(logger:error("error!"))
 
 print("Socket Logging OK")
 
