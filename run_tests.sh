91246d80d163ca4da6b7d630a0e98f8f8f209a7a
Max Chase <max.chase@gmail.com>
2017-04-14 23:44:52 -0400

    Move netcat handling into test script.

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 #
 
+nc -k -l 5000 & NC_PID=$!
+
 LUA_PATH="src/?.lua;$LUA_PATH" lua -lluacov tests/test.lua
 test_exit_code=$?
+
+kill $NC_PID
 
 rm -f test.db test.log*
 exit "$test_exit_code"
