df0ff697f80c9c847ae0de98d46a5e5fcd922f58
Max Chase <max.chase@gmail.com>
2017-04-14 21:52:19 -0400

    Let's put it back, and bind to 0.0.0.0 on the LISTENER, whatever that ends up being.

--- tests/testSocket.lua
+++ tests/testSocket.lua
@@ -1,6 +1,6 @@
 local log_sock = require"logging.socket"
 
-local logger = log_sock("0.0.0.0", 5000)
+local logger = log_sock("localhost", 5000)
 
 assert(logger:info("logging.socket test"))
 assert(logger:debug("debugging..."))
