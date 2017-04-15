a220c055d92ee12be291105000a2e16283f1b3f0
Max Chase <max.chase@gmail.com>
2017-04-14 23:47:27 -0400

    Cover lack of server.

--- tests/testSocket.lua
+++ tests/testSocket.lua
@@ -6,5 +6,11 @@ assert(logger:info("logging.socket test"))
 assert(logger:debug("debugging..."))
 assert(logger:error("error!"))
 
+local bad_logger = log_sock("localhost", 5001)
+
+assert(not bad_logger:info("logging.socket test"))
+assert(not bad_logger:debug("debugging..."))
+assert(not bad_logger:error("error!"))
+
 print("Socket Logging OK")
 
