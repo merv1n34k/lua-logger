a1879a8d556113388d04e0d38a4b06cd76106180
Max Chase <max.chase@gmail.com>
2017-04-15 11:33:37 -0400

    That header table... wasn't.

--- tests/testMail.lua
+++ tests/testMail.lua
@@ -3,21 +3,30 @@ local log_email = require"logging.email"
 local logger = log_email {
 	rcpt = "mail@localhost",
 	from = "mail@localhost",
-	{
+	headers = {
 		subject = "[%level] logging.email test",
-	}, -- headers
+	},
 }
 
 assert(logger:info("logging.email test"))
 assert(logger:debug("debugging..."))
 assert(logger:error("error!"))
 
+local no_header_logger = log_email {
+    rcpt = "mail@localhost",
+    from = "mail@localhost",
+}
+
+assert(no_header_logger:info("logging.email test"))
+assert(no_header_logger:debug("debugging..."))
+assert(no_header_logger:error("error!"))
+
 local bad_logger = log_email {
     rcpt = "mail@5",
     from = "mail@5",
-    {
+    headers = {
         subject = "[%level] logging.email test",
-    }, -- headers
+    },
 }
 
 assert(not bad_logger:info("logging.email test"))
