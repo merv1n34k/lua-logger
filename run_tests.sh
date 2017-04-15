2b77b6ea70de53534ef6910e9e46984d9643e810
Max Chase <max.chase@gmail.com>
2017-04-15 00:09:55 -0400

    Attempt to set up dummy email server

--- run_tests.sh
+++ run_tests.sh
@@ -2,11 +2,13 @@
 #
 
 nc -k -l 5000 & NC_PID=$!
+python -m smtpd -n -c DebuggingServer localhost:25 & SMTP_PID=$!
 
 LUA_PATH="src/?.lua;$LUA_PATH" lua -lluacov tests/test.lua
 test_exit_code=$?
 
 kill $NC_PID
+kill $SMTP_PID
 
 rm -f test.db test.log*
 exit "$test_exit_code"
