3986aaf6ce26653a49977e257e686b9b2c25f6b4
Max Chase <max.chase@gmail.com>
2017-04-15 21:49:59 -0400

    Test self-reference.

--- tests/testLibrary.lua
+++ tests/testLibrary.lua
@@ -2,3 +2,9 @@ local lib = require"logging"
 
 assert(lib.tostring{1, 2, 3, 4, 5, 6, 7, 8, 9} == "{1, 2, 3, 4, 5, 6, 7, 8, 9}", "test is incorrect")
 assert(lib.tostring{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} == "{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}", "numbers were reordered")
+
+local self_ref = {}
+
+self_ref[self_ref] = self_ref
+
+lib.tostring(self_ref)
