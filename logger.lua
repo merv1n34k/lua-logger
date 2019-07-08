43836402783f13a613df8818eaa90e0b6b48b357
Doron Behar <doron.behar@gmail.com>
2019-07-08 10:01:53 +0300

    Make logger fit what's described in README

--- logger.lua
+++ logger.lua
@@ -1,21 +1,21 @@
 local DEFAULT_LEVELS = {
-	-- The highest possible rank and is intended to turn off logging.
-	"OFF",
-	-- Severe errors that cause premature termination. Expect these to be immediately visible on a status console.
-	"FATAL",
-	-- Other runtime errors or unexpected conditions. Expect these to be immediately visible on a status console.
-	"ERROR",
-	-- Use of deprecated APIs, poor use of API, 'almost' errors, other runtime situations that are undesirable or
-	-- unexpected, but not necessarily "wrong". Expect these to be immediately visible on a status console.
-	"WARN",
-	-- Interesting runtime events (startup/shutdown). Expect these to be immediately visible on a console, so be
-	-- conservative and keep to a minimum.
-	"INFO",
+	-- Most detailed information. Expect these to be written to logs only
+	"TRACE",
 	-- Detailed information on the flow through the system. Expect these to be written to logs only. Generally speaking,
 	-- most lines logged by your application should be written as DEBUG.
 	"DEBUG",
-	-- Most detailed information. Expect these to be written to logs only
-	"TRACE"
+	-- Interesting runtime events (startup/shutdown). Expect these to be immediately visible on a console, so be
+	-- conservative and keep to a minimum.
+	"INFO",
+	-- Use of deprecated APIs, poor use of API, 'almost' errors, other runtime situations that are undesirable or
+	-- unexpected, but not necessarily "wrong". Expect these to be immediately visible on a status console.
+	"WARN",
+	-- Other runtime errors or unexpected conditions. Expect these to be immediately visible on a status console.
+	"ERROR",
+	-- Severe errors that cause premature termination. Expect these to be immediately visible on a status console.
+	"FATAL",
+	-- The highest possible rank and is intended to turn off logging.
+	"OFF",
 }
 
 local function indexof(val, t)
@@ -34,7 +34,9 @@ end
 -------------------------------------------------------------------------------
 return function(append, settings)
 	if type(append) ~= "function" then
-		return nil, "Appender must be a function."
+		append = function(self, level, message)
+			io.stderr:write(level .. '\t' .. message .. '\n')
+		end
 	end
 
 	local logger = {}
@@ -47,7 +49,7 @@ return function(append, settings)
 	setmetatable(settings, {
 		__index = {
 			levels = DEFAULT_LEVELS,
-			init_level = DEFAULT_LEVELS[6]
+			init_level = DEFAULT_LEVELS[1]
 		}
 	})
 	logger.levels = settings.levels
@@ -90,7 +92,7 @@ return function(append, settings)
 		elseif type(level) == "string" then
 			order = indexof(level, self.levels)
 		end
-		if order <= self.level_order then
+		if order >= self.level_order then
 			return self:append(level, msg)
 		else
 			return
